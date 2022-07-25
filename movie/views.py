1e9553a1e50d8f35cb895d9d5824b37b9691e1be
1saifj <saifalialjanahi@gmail.com>
2022-07-25 14:43:05 +0300

    add signup page

--- movie/views.py
+++ movie/views.py
@@ -1,15 +1,20 @@
+import email
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 
 def home(request):
-    return HttpResponse(
-        '<h1>Hello World!</h1>'
-    )
+    searchTerm = request.GET.get('searchMovie')
+    return render(request, 'home.html', {'searchTerm': searchTerm})
 
 
 def about(request):
     return HttpResponse(
         '<h1>About</h1>'
     )
+
+
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email': email})
--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -1,4 +1,5 @@
 
+from unicodedata import name
 from django.contrib import admin
 from django.urls import path
 from movie import views as movieViews
@@ -6,5 +7,6 @@ from movie import views as movieViews
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',movieViews.home),
-    path('about/',movieViews.about)
+    path('about/',movieViews.about),
+    path('signup/',movieViews.signup, name='signup'),
 ]
