c5c9a4fba25fb6272732ad3011fe1aaffc4d22c7
1saifj <saifalialjanahi@gmail.com>
2022-07-25 15:10:54 +0300

    add urlpatterns to locate media

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -1,12 +1,16 @@
 
 from unicodedata import name
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from movie import views as movieViews
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',movieViews.home, name='home'),
     path('about/',movieViews.about),
     path('signup/',movieViews.signup, name='signup'),
 ]
+
+urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
