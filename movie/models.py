3e7819586e75311f4cd8c7dc4d512f41133619c8
1saifj <saifalialjanahi@gmail.com>
2022-07-25 14:59:24 +0300

    movie model

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie (models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    image = models.ImageField(upload_to='movie/images/')
+    url = models.URLField(max_length=200, blank=True)
--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -6,7 +6,7 @@ from movie import views as movieViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',movieViews.home),
+    path('',movieViews.home, name='home'),
     path('about/',movieViews.about),
     path('signup/',movieViews.signup, name='signup'),
 ]
